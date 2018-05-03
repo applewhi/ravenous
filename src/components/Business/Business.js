16a90adf6cb11b5aeb536200a1f78de44fad519e
applewhi <38227492+applewhi@users.noreply.github.com>
2018-05-03 00:42:48 -0700

    Added this.props to business object properties

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,23 +1,23 @@
-imprt React from 'react';
+import React from 'react';
 
 class Business extends React.Component{
   render(){
     return(
-      <div className="Business">
-        <div className="image-container">
-          <img src={business.imageSrc} alt=''/>
+      <div classname="Business">
+        <div classname="image-container">
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>{business.name}</h2>
-        <div className="Business-information">
-          <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state +' '+business.zipCode}</p>
+        <h2>{this.props.business.name}</h2>
+        <div classname="Business-information">
+          <div classname="Business-address">
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state +' '+this.props.business.zipCode}</p>
           </div>
-          <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating} stars</h3>
-            <p>{business.reviewCount} reviews</p>
+          <div classname="Business-reviews">
+            <h3>{this.props.business.category}</h3>
+            <h3 classname="rating">{this.props.business.rating} stars</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
