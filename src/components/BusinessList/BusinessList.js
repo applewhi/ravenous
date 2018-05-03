c6a48db459cb3455da60c09a3e2abcef8d3d9e1a
applewhi <38227492+applewhi@users.noreply.github.com>
2018-05-02 23:47:54 -0700

    Update BusinessList.js

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,15 @@
+render(){
+    return (
+      <div classname="BusinessList">
+        <Busisness />
+        <Busisness />
+        <Busisness />
+        <Busisness />
+        <Busisness />
+        <Busisness />
+      </div>
+    )
+  }
+}
+
+export default BusinessList;
