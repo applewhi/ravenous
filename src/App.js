92e9b078c96ae8c035cd7003e9cae8c87cc22ea7
applewhi <38227492+applewhi@users.noreply.github.com>
2018-05-03 00:04:52 -0700

    Pasted businesss object here and created array

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const business = {
+ imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+ name: 'MarginOtto Pizzeria',
+ address: '1010 Paddington Way',
+ city: 'Flavortown',
+ state: 'NY',
+ zipCode: '10101',
+ category: 'Italian',
+ rating: 4.5,
+ reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+
+];
+
 class App extends Component {
   render() {
+    <BusinessList businesses={business} />
     return (
       <div className="App">
         <header className="App-header">
